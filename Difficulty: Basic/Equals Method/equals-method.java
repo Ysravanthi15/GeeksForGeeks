20f59413357f7d6bbb56a39598534ef63b017000
Sravanthi Yedla <167872100+Ysravanthi15@users.noreply.github.com>
2026-03-28 11:14:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Equals Method/equals-method.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+class Solution {
+    public static boolean areEqual(int a[], int b[]) {
+        // Your code here
+        if(a[0]==b[0]){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
